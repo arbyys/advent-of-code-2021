f95b5165a348ddbf2c7739a9f63812a5145d2393
arbyys <adam.petricek@centrum.cz>
2021-12-03 15:17:34 +0100

    day2

---
+++ day-2/part1.py
@@ -0,0 +1,19 @@
+from aocd import get_data
+
+data = get_data(day=2, year=2021)
+data = data.splitlines()
+#data = [*map(int, data)]
+
+coords = [0,0]
+for instruction in data:
+    command = instruction.split(" ")[0]
+    amount = int(instruction.split(" ")[1])
+
+    if(command == "forward"):
+        coords[0] += amount
+    elif(command == "down"):
+        coords[1] += amount
+    else:
+        coords[1] -= amount
+
+print(coords[0] * coords[1])
---
+++ day-2/part2.py
@@ -0,0 +1,20 @@
+from aocd import get_data
+
+data = get_data(day=2, year=2021)
+data = data.splitlines()
+#data = [*map(int, data)]
+
+coords = [0,0,0]
+for instruction in data:
+    command = instruction.split(" ")[0]
+    amount = int(instruction.split(" ")[1])
+
+    if(command == "forward"):
+        coords[0] += amount
+        coords[1] += coords[2] * amount
+    elif(command == "down"):
+        coords[2] += amount
+    else: # up
+        coords[2] -= amount
+
+print(coords[0] * coords[1])
