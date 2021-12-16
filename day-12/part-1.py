e8f026f860570a3ac74c25a5eb4f7cfd4c92cc52
arbyys <adam.petricek@centrum.cz>
2021-12-16 20:29:51 +0100

    day12

--- day-12/part-1.py
+++ day-12/part-1.py
@@ -1,34 +1,5 @@
 from aocd import get_data
+from collections import defaultdict
 
 data = get_data(day=12, year=2021)
 data = data.splitlines()
-
-graph = {}
-pointers = {}
-
-for i,instruction in enumerate(data):
-    instruction = instruction.split("-")
-    if instruction[0] not in graph:
-        graph[instruction[0]] = []
-    for subInstruction in instruction[1]:
-        if(subInstruction not in graph):
-            graph[subInstruction] = []
-    graph[instruction[0]].append(instruction[1])
-    for subInstruction in instruction[1]:
-            graph[subInstruction].append(instruction)
-
-for i, (key, value) in enumerate(graph.items()):
-   print(i, key, value)
-
-visitedList = [[]]
-
-def depthFirst(graph, currentVertex, visited):
-    visited.append(currentVertex)
-    for vertex in graph[currentVertex]:
-        if vertex not in visited:
-            depthFirst(graph, vertex, visited.copy())
-    visitedList.append(visited)
-
-depthFirst(graph, "start", [])
-
-print(visitedList)
