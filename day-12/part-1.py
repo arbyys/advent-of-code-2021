dc8af71b39313c1ec4272ecb810b28021bd6360a
arbyys <adam.petricek@centrum.cz>
2021-12-16 20:15:24 +0100

    vanocniuloha

--- day-12/part-1.py
+++ day-12/part-1.py
@@ -4,26 +4,31 @@ data = get_data(day=12, year=2021)
 data = data.splitlines()
 
 graph = {}
-for instruction in data:
+pointers = {}
+
+for i,instruction in enumerate(data):
     instruction = instruction.split("-")
     if instruction[0] not in graph:
         graph[instruction[0]] = []
-
+    for subInstruction in instruction[1]:
+        if(subInstruction not in graph):
+            graph[subInstruction] = []
     graph[instruction[0]].append(instruction[1])
+    for subInstruction in instruction[1]:
+            graph[subInstruction].append(instruction)
 
-def dfs(adj_list, start, target, path, visited = set()):
-    path.append(start)
-    visited.add(start)
-    if start == target:
-        return path
-    for neighbour in adj_list[start]:
-        if neighbour not in visited:
-            result = dfs(adj_list, neighbour, target, path, visited)
-            if result is not None:
-                return result
-	  path.pop()
-    return None
+for i, (key, value) in enumerate(graph.items()):
+   print(i, key, value)
 
-traversal_path = []
-traversal_path = dfs(adj_list, 0, 3, traversal_path)
-print(traversal_path)
+visitedList = [[]]
+
+def depthFirst(graph, currentVertex, visited):
+    visited.append(currentVertex)
+    for vertex in graph[currentVertex]:
+        if vertex not in visited:
+            depthFirst(graph, vertex, visited.copy())
+    visitedList.append(visited)
+
+depthFirst(graph, "start", [])
+
+print(visitedList)
