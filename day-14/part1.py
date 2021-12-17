f062e04af0ae4b855fd0342b67d3ff5b1a5f6bb2
arbyys <adam.petricek@centrum.cz>
2021-12-17 16:40:53 +0100

    day14-part1

---
+++ day-14/part1.py
@@ -0,0 +1,38 @@
+from aocd import get_data
+
+data = get_data(day=14, year=2021)
+
+template = list(data.split("\n\n")[0])
+rules = data.split("\n\n")[1].splitlines()
+
+matchingLetters = []
+appendingLetters = []
+
+for rule in rules:
+    currentRule = rule.split(" -> ")
+    matchingLetters.append(currentRule[0])
+    appendingLetters.append(currentRule[1])
+
+NUMBER_OF_STEPS = 10
+for step in range(1, NUMBER_OF_STEPS+1):
+    workingTemplate = template.copy()
+    workingIndex = 0
+    for index,letter in enumerate(template):
+        if(index >= len(template)-1):
+            break
+        nextLetter = template[index+1]
+        if(letter+nextLetter in matchingLetters):
+            indexOfLetter = matchingLetters.index(letter+nextLetter)
+            workingTemplate.insert(workingIndex+1, appendingLetters[indexOfLetter])
+            workingIndex += 1
+        workingIndex += 1
+    template = workingTemplate.copy()
+
+occurences = {}
+for letter in template:
+    if(letter not in occurences):
+        occurences[letter] = 1
+        continue
+    occurences[letter] += 1
+
+print(max(occurences.values()) - min(occurences.values()))
