fe470f71a4e6cfea3ec0cf4e52431a1578d22f4a
arbyys <adam.petricek@centrum.cz>
2021-12-21 00:50:57 +0100

    day15 init, day 16part1

---
+++ day-15/part2.py
@@ -0,0 +1,8 @@
+from aocd import get_data
+
+data = get_data(day=1, year=2021)
+data = data.splitlines()
+#data = [*map(int, data)]
+
+#with open("input.txt") as file:
+#    data = file.read().splitlines()
